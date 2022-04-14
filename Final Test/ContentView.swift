915969888a1200edbd1eb16390daa1d3ab1a577b
Lekhana Sandeep <lekhanasandeep2018@gmail.com>
2022-04-14 20:04:59 +0530

    Initial Commit

---
+++ Final Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Final Test
+//
+//  Created by Lekhana Sandeep on 2022-04-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final Test/Final_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_TestApp.swift
+//  Final Test
+//
+//  Created by Lekhana Sandeep on 2022-04-14.
+//
+
+import SwiftUI
+
+@main
+struct Final_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
